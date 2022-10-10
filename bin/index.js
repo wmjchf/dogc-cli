0a8291e8ceede223db2d79d4c4608cf71ecd8609
wmjchf <3284304056@qq.com>
2022-10-10 23:26:17 +0800

    feat: 增加默认description

--- bin/index.js
+++ bin/index.js
@@ -16,7 +16,7 @@ program.version(version).option("-v,--version");
 program
   .command("create <app-name>")
   .description("create a new project")
-  .option("-d,--descritption <description>", "add a detail descript")
+  .option("-d,--description <description>", "add a detail descript")
   .action((appName, options) => {
     // 创建操作的逻辑，这里会以问题询问的方式获取项目信息，见第二步的方法
     create(appName, options);
